424ed5b6b3e9a0faacde1cbbe7fa01a4db6a4b4d
unknown <105841112821@student.unismuh.ac.id>
2024-06-23 20:52:24 +0800

    import/Eksport Component

--- App.js
+++ App.js
@@ -1,18 +1,107 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import React from 'react'
-const App = () => {
-  return (
-    <View style= {{flex: 1}}>
-      <View style= {{flex: 1,flexDirection: 'row'}}>
-   <View style= {{flex: 1,backgroundColor:'green'}}></View>
-   <View style= {{flex: 1,backgroundColor:'yellow'}}></View>
-    </View>
-    <View style= {{flex: 1,flexDirection: 'colum'}}>
-   <View style= {{flex: 1,backgroundColor:'red'}}></View>
-   <View style= {{flex: 1,backgroundColor:'blue'}}></View>
-    </View>
+import { StyleSheet, Text, View, Image, ScrollView }from 'react-native';
+import React from 'react';
 
-    </View>
-  );
-}
-export default App
+const App = () => {
+ return (
+  <View style={{
+    flex: 1,
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }}>
+    <Image
+    source={require('./assets/hijau.jpg')}
+    style={{
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      top: 15,
+      opacity: 0.4,
+      zIndex: -1,
+    }}
+    resizeMode="cover"
+    />
+      <ScrollView contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+      }}>
+        <Image
+        source={require('./assets/spotify.png')}
+        style={{
+          width: 150,
+          height: 150,
+          marginTop: 50,
+          opacity: 1,
+          zIndex: 1,
+        }}
+        resizeMode="cover"
+        />
+        <Text style={{
+          fontSize: 20,
+          fontFamily: 'sans-serif',
+          fontWeight: 'bold',
+          color: 'white',
+          position: 'absolute',
+          top: 0,
+          marginTop: 300,
+        }}>Selamat datang di Spotify</Text>
+
+        <Text style={{
+          color: 'white',
+          fontSize: 18,
+          textAlign: 'center',
+          marginTop: 30,
+        }}>layanan musik digital, podcast, dan video yang memberimu akses ke jutaan lagu dan konten lain dari kreator di seluruh dunia. </Text>
+          
+          <Text style={{
+            color: 'white',
+            fontSize: 18,
+            textAlign: 'center',
+            marginTop: 20,
+          }}>Layanan Spotify Premium menawarkan manfaat tambahan dibandingkan dengan versi gratis.</Text>
+           
+           <View style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+            marginBottom: 40,
+           }}>
+            <View style={{ marginHorizontal: 10 }}>
+              <Text style={{
+                width: 100,
+                height: 50,
+                color: 'white',
+                backgroundColor: 'blue',
+                textAlign: 'center',
+                lineHeight: 50,
+                fontSize: 20,
+                borderRadius: 10,
+                marginTop: 80,
+              }}>Login</Text>
+              </View>
+              <View style={{ marginHorizontal: 20}}>
+                <Text style={{
+                  width: 100,
+                  height: 50,
+                  color: 'white',
+                  backgroundColor: 'green',
+                  textAlign: 'center',
+                  lineHeight: 50,
+                  fontSize: 20,
+                  borderRadius: 10,
+                  marginTop: 80,
+                }}>Register</Text>
+                </View>
+                </View>
+                </ScrollView>
+                </View>
+                );
+              }
+
+                export default App;
+
+
+    
