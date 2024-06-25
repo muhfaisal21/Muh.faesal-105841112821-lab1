70570c6f4d803338aff76c97138c79a7038ed8e3
my-faisal <105841112821@student.unismuh.ac.id>
2024-06-25 13:29:44 +0800

    Tugas Lab1

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View, Image, ScrollView }from 'react-native';
 import React from 'react';
+import ButtonComponent from './component/button/button';
 
 const App = () => {
  return (
@@ -32,7 +33,7 @@ const App = () => {
         style={{
           width: 150,
           height: 150,
-          marginTop: 50,
+          marginTop: 20,
           opacity: 1,
           zIndex: 1,
         }}
@@ -45,14 +46,14 @@ const App = () => {
           color: 'white',
           position: 'absolute',
           top: 0,
-          marginTop: 300,
-        }}>Selamat datang di Spotify</Text>
+          marginTop: 325,
+        }}>Welcome to Spotify</Text>
 
         <Text style={{
           color: 'white',
           fontSize: 18,
           textAlign: 'center',
-          marginTop: 30,
+          marginTop: 20,
         }}>layanan musik digital, podcast, dan video yang memberimu akses ke jutaan lagu dan konten lain dari kreator di seluruh dunia. </Text>
           
           <Text style={{
@@ -63,45 +64,21 @@ const App = () => {
           }}>Layanan Spotify Premium menawarkan manfaat tambahan dibandingkan dengan versi gratis.</Text>
            
            <View style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
             marginTop: 20,
             marginBottom: 40,
            }}>
-            <View style={{ marginHorizontal: 10 }}>
-              <Text style={{
-                width: 100,
-                height: 50,
-                color: 'white',
-                backgroundColor: 'blue',
-                textAlign: 'center',
-                lineHeight: 50,
-                fontSize: 20,
-                borderRadius: 10,
-                marginTop: 80,
-              }}>Login</Text>
-              </View>
-              <View style={{ marginHorizontal: 20}}>
-                <Text style={{
-                  width: 100,
-                  height: 50,
-                  color: 'white',
-                  backgroundColor: 'green',
-                  textAlign: 'center',
-                  lineHeight: 50,
-                  fontSize: 20,
-                  borderRadius: 10,
-                  marginTop: 80,
-                }}>Register</Text>
-                </View>
+                <ButtonComponent title="Sign In" color="green"/>
+                <ButtonComponent title="Sign Up" color="blue"/>
                 </View>
                 </ScrollView>
                 </View>
                 );
               }
 
-                export default App;
+export default App;
 
 
     
